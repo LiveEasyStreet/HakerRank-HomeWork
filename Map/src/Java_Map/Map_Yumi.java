325edc558eede0a69a7c80a466309ba7c8624a16
Yumi <spr1238@gmail.com>
2023-02-09 18:55:47 +0900

    yumi 과제 제출
    1) src 폴더 source loot로 바꿈
    2) 하위에 Map_Yumi.java, result, Solution.java 파일 생성하여 문제풀이    등록
    
    Resolve : #9

---
+++ Map/src/Java_Map/Map_Yumi.java
@@ -0,0 +1,21 @@
+// 작성자 : 하유미
+// 작성일 : 23-02-09 17:44
+//Complete this code or write your own from scratch
+package Java_Map;
+import java.util.*;
+
+public class Map_Yumi {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < n; i++) {
+            String name = in.nextLine();
+            int phone = in.nextInt();
+            in.nextLine();
+        }
+        while (in.hasNext()) {
+            String s = in.nextLine();
+        }
+    }
+}
