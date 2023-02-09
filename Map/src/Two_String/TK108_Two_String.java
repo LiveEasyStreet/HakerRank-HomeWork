9375031bd9e9b6d84cc33897250f506582e6c83f
TaeKyoung108 <rlaxorud0108@naver.com>
2023-02-09 17:27:55 +0900

    과제할당
    Java Map 수행 완료
    
    #6

---
+++ Map/src/Two_String/TK108_Two_String.java
@@ -0,0 +1,66 @@
+package src.Two_String;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static String twoStrings(String s1, String s2) {
+        Map<Character, Integer> map_t=new HashMap<>();
+        for(int i=0;i<s1.length();i++){
+            map_t.put(s1.charAt(i),1);
+        }
+        for(int i=0;i<s2.length();i++){
+            if(map_t.containsKey(s2.charAt(i))){
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+}
+
+public class TK108_Two_String {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+
+                String s2 = bufferedReader.readLine();
+
+                String result = Result.twoStrings(s1, s2);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
