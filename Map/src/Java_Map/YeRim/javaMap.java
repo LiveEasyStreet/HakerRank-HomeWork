26a7bbf1f170416b585dda8f312984af669f3717
DESKTOP-HTD9PCN\user2 <leeyl103@naver.com>
2023-02-13 17:22:25 +0900

    예림 Map 과제 제출!!

---
+++ Map/src/Java_Map/YeRim/javaMap.java
@@ -0,0 +1,25 @@
+package src.Java_Map.YeRim;
+
+import java.util.*;
+import java.io.*;
+
+public class javaMap {
+    public static void main(String []argh)
+    {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.nextLine();
+        for(int i=0;i<n;i++)
+        {
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            in.nextLine();
+        }
+        while(in.hasNext())
+        {
+            String s=in.nextLine();
+        }
+    }
+}
+
+
---
+++ Map/src/Two_String/YeRIm/Solution.java
@@ -0,0 +1,64 @@
+package src.Two_String.YeRIm;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    static String twoStrings(String s1, String s2) {
+        Set<String> set = new HashSet<String>();
+        String[] c1 = s1.split("");
+        for (String a : c1)
+            set.add(a);
+        for (String a : set)
+            if (s2.contains(a))
+                return "YES";
+        return "NO";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+
+                String s2 = bufferedReader.readLine();
+
+                String result = Result.twoStrings(s1, s2);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
