776ee83f73e81748e13cf4f898c859787ad84cbf
jungwon <geomusteel@naver.com>
2023-02-18 05:20:47 +0900

    정원
    #41

---
+++ src/HW_23_02_17_Stack_Queue/JungWon1.java
@@ -0,0 +1,54 @@
+package HW_23_02_17_Stack_Queue;
+
+import java.util.*;
+
+public class JungWon1 {
+
+    Stack<Character> st = new Stack<>();
+
+    public void pushCharacter(char c) {
+        st.push(c);
+    }
+
+    public char popCharacter() {
+        return st.pop();
+    }
+
+    Queue<Character> qu = new LinkedList<>();
+
+    public void enqueueCharacter(char c) {
+        qu.add(c);
+    }
+
+    public char dequeueCharacter() {
+        return qu.poll();
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+
+        char[] s = input.toCharArray();
+
+        JungWon1 p = new JungWon1();
+
+        for (char c : s) {
+            p.pushCharacter(c);
+            p.enqueueCharacter(c);
+        }
+
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length/2; i++) {
+            if (p.popCharacter() != p.dequeueCharacter()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println( "The word, " + input + ", is "
+                + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+    }
+
+}
+
---
+++ src/HW_23_02_17_Stack_Queue/JungWon2.java
@@ -0,0 +1,59 @@
+package HW_23_02_17_Stack_Queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+class JungWon2 {
+
+    public static void main(String[] argh) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+
+            Deque<Character> stack = new ArrayDeque<>();
+
+            boolean tempBool = true;
+
+            String input = sc.nextLine();
+
+            for (int i = 0; i < input.length(); i++) {
+
+                char temp = input.charAt(i);
+                if (temp == '(' || temp == '[' || temp == '{') {
+                    stack.addLast(temp);
+                    continue;
+                }
+
+                if (stack.size() == 0) {
+                    tempBool = false;
+                    break;
+                }
+
+                char temp2 = stack.pollLast();
+                if (temp == ')' & temp2 != '(') {
+                    tempBool = false;
+                    break;
+                }
+                if (temp == ']' & temp2 != '[') {
+                    tempBool = false;
+                    break;
+                }
+                if (temp == '}' & temp2 != '{') {
+                    tempBool = false;
+                    break;
+                }
+
+            }
+
+            if (stack.size() > 0) {
+                tempBool = false;
+            }
+
+            System.out.println(tempBool);
+
+        }
+        sc.close();
+
+    }
+}
